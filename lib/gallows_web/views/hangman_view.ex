e0ffc83171a3fc8378c552820bed4fb38865cc8f
Mike Over <mike.over@teamsnap.com>
2017-10-11 13:23:54 -0400

    Add helpers for displaying alert for game state, form for new game when game ends

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,7 +1,13 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
-  def word_so_far(tally) do
-    tally.letters |> Enum.join(" ")
+  import GallowsWeb.Views.Helpers.GameStateHelper
+
+  def game_over?(%{ game_state: game_state }) do
+    game_state in [ :won, :lost ]
+  end
+
+  def new_game_button(conn) do
+    button("New Game", to: hangman_path(conn, :create_game))
   end
 end
